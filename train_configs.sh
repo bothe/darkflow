d3fde736bfa7e40314ad6e88f5cfc0af06c735b1
mohamed mosad <mohamedmosad209@gmail.com>
2020-01-29 18:49:53 +0200

    added single quotes for links

--- train_configs.sh
+++ train_configs.sh
@@ -2,12 +2,12 @@
 mkdir bin
 cd bin/
 echo "download pretrained tiny yolo voc weights"
-wget https://pjreddie.com/media/files/yolov2-tiny-voc.weights
+wget 'https://pjreddie.com/media/files/yolov2-tiny-voc.weights'
 cd ../
 echo "downloading training data"
 # images
-wget http://download1646.mediafire.com/ux1iy7ebzpgg/2okuftipzimoemk/images.tar.gz
-wget http://download1496.mediafire.com/9fqwysedd6sg/o0dsih49vgllcle/annotations.tar.gz
+wget 'http://download1646.mediafire.com/ux1iy7ebzpgg/2okuftipzimoemk/images.tar.gz'
+wget 'http://download1496.mediafire.com/9fqwysedd6sg/o0dsih49vgllcle/annotations.tar.gz'
 echo "extracting training data"
 tar -xvf images.tar.gz
 tar -xvf annotations.tar.gz
