181f7665a766e43c3584f336c1b0445c82c71f93
mohamed209 <mohamedmosad209@gmail.com>
2020-01-29 16:55:16 +0200

    made train configs pull data from remote servers

--- train_configs.sh
+++ train_configs.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 mkdir bin
 cd bin/
-# download pretrained tiny yolo voc weights
+echo "download pretrained tiny yolo voc weights"
 wget https://pjreddie.com/media/files/yolov2-tiny-voc.weights
 cd ../
-# checkpoints directory
+echo "downloading training data"
+# images
+wget http://download1646.mediafire.com/shfpg3rzw0og/2okuftipzimoemk/images.tar.gz
+wget wget http://download1496.mediafire.com/9fqwysedd6sg/o0dsih49vgllcle/annotations.tar.gz
+echo "extracting training data"
+tar -xvzf images.tar.gz
+tar -xvzf annotations.tar.gz
+rm -r images.tar.gz
+rm -r annotations.tar.gz
+echo "making checkpoints directory"
 mkdir ckpt
-# install darkflow
+echo "installing darkflow"
 pip install .
 pip install -e .
+
+echo "start training :)"
+python train.py
