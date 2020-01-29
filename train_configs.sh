f80ce82e468028eab1e96045fcb96b4d722c738e
mohamed209 <mohamedmosad209@gmail.com>
2020-01-29 18:46:14 +0200

    fixed download links

--- train_configs.sh
+++ train_configs.sh
@@ -6,11 +6,11 @@ wget https://pjreddie.com/media/files/yolov2-tiny-voc.weights
 cd ../
 echo "downloading training data"
 # images
-wget http://download1646.mediafire.com/bi6g2z6w3rsg/2okuftipzimoemk/images.tar.gz
+wget http://download1646.mediafire.com/ux1iy7ebzpgg/2okuftipzimoemk/images.tar.gz
 wget http://download1496.mediafire.com/9fqwysedd6sg/o0dsih49vgllcle/annotations.tar.gz
 echo "extracting training data"
-tar -xvzf images.tar.gz
-tar -xvzf annotations.tar.gz
+tar -xvf images.tar.gz
+tar -xvf annotations.tar.gz
 rm -r images.tar.gz
 rm -r annotations.tar.gz
 echo "making checkpoints directory"
