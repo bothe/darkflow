07569faa188ae724d5d703be4e128ff34a903ea3
mohamed mosad <mohamedmosad209@gmail.com>
2020-01-31 22:27:26 +0200

    read from correct labels

--- train_test_split.py
+++ train_test_split.py
@@ -5,14 +5,14 @@ import sys
 
 def train_test_split(samples):
     images=os.listdir('images/')
-    labels=os.listdir('annotations/')
+    labels=os.listdir('c_annotations/')
     all_data=list(zip(sorted(images),sorted(labels)))
     random.shuffle(all_data)
     i=0
     for element in all_data:
         if i<samples:
             shutil.copy('images/'+element[0],'train-images/')
-            shutil.copy('annotations/'+element[1],'train-labels/')
+            shutil.copy('c_annotations/'+element[1],'train-labels/')
             i+=1
         else:
             return
