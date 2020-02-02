e98bbac6713b589a01e015445be11e29cc52a141
mohamed mosad <mohamedmosad209@gmail.com>
2020-02-02 20:15:27 +0200

    20 epoches

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import cv2
 options = {"model": "cfg/custom-yolov2.cfg", 
            "load": "bin/yolov2.weights",
            "batch": 8,
-           "epoch": 50,
+           "epoch": 20,
            'momentum': 0.9,
            "gpu": 1.0,
            "train": True,
