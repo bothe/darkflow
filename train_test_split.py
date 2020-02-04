f5766144fd76a2e1b0df026042e58cde692dc6db
mohamed mosad <mohamedmosad209@gmail.com>
2020-02-04 12:33:12 +0200

    train test split with ratio

--- train_test_split.py
+++ train_test_split.py
@@ -3,19 +3,19 @@ import shutil
 import random
 import sys
 
-def train_test_split(samples):
-    images=os.listdir('images/')
-    labels=os.listdir('c_annotations/')
-    all_data=list(zip(sorted(images),sorted(labels)))
+
+def train_test_split(ratio):
+    images = os.listdir('images/')
+    labels = os.listdir('c_annotations/')
+    all_data = list(zip(sorted(images), sorted(labels)))
     random.shuffle(all_data)
-    i=0
-    for element in all_data:
-        if i<samples:
-            shutil.copy('images/'+element[0],'train-images/')
-            shutil.copy('c_annotations/'+element[1],'train-labels/')
-            i+=1
-        else:
-            return
+    for element in all_data[:int(ratio*len(images))]:
+        shutil.copy('images/'+element[0], 'train-images/')
+        shutil.copy('c_annotations/'+element[1], 'train-labels/')
+    for element in all_data[int(ratio*len(images)):]:
+        shutil.copy('images/'+element[0], 'test-images/')
+        shutil.copy('c_annotations/'+element[1], 'test-labels/')
+
+
 if __name__ == "__main__":
     train_test_split(int(sys.argv[1]))
-    
