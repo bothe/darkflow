a950c607a2d2fd15dd48610be363ad46a1e82042
Chandrakant Bothe <bothe@foviatech.com>
2020-02-05 15:07:03 +0100

    mechanism for initializing or loading the weights and base_data_dir

--- train.py
+++ train.py
@@ -1,17 +1,32 @@
+import os
+
 from darkflow.net.build import TFNet
 
-options = {"model": "cfg/custom-yolov2.cfg",
-           # "load": "bin/yolov2.weights",
-           'load': -1,
+# Directory of the data to be trained (should contain: custom-yolov2.cfg, annotations, images, logs, and ckpt)
+base_data_dir = "data_valves"
+
+# Check if already trained
+if os.path.exists(base_data_dir + "/ckpt/checkpoint"):
+    load = -1
+else:
+    load = "bin/yolov2.weights"
+    if not os.path.exists(load):
+        print("bin/yolov2.weights is not available, initializing weights from scratch!")
+        load = 0
+
+options = {"model": base_data_dir + "/custom-yolov2.cfg",
+           'load': load,
            "batch": 8,
            "epoch": 50,
            'momentum': 0.9,
-           'summary': 'logs/',
+           'summary': base_data_dir + "/logs/",
+           "backup": base_data_dir + "/ckpt/",
            "gpu": 1.0,
            "train": True,
-           "save": 100,
-           "annotation": "annotations/",
-           "dataset": "images/"}
+           "save": 16,
+           "annotation": base_data_dir + "/annotations/",
+           "dataset": base_data_dir + "/images/"}
+
 # load network
 tfnet = TFNet(options)
 # start train
