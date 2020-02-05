26886b0fa5b7fe0aa9c9bef33f3e9f247f2919d3
Chandrakant Bothe <bothe@foviatech.com>
2020-02-05 13:46:08 +0100

    path change and refactoring

--- train.py
+++ train.py
@@ -1,16 +1,17 @@
-import matplotlib.pyplot as plt
-import numpy as np
 from darkflow.net.build import TFNet
-import cv2
-options = {"model": "cfg/custom-yolov2.cfg", 
-           "load": "bin/yolov2.weights",
+
+options = {"model": "cfg/custom-yolov2.cfg",
+           # "load": "bin/yolov2.weights",
+           'load': -1,
            "batch": 8,
            "epoch": 50,
            'momentum': 0.9,
+           'summary': 'logs/',
            "gpu": 1.0,
            "train": True,
-           "annotation": "./train-labels/",
-           "dataset": "./train-images/"}
+           "save": 100,
+           "annotation": "annotations/",
+           "dataset": "images/"}
 # load network
 tfnet = TFNet(options)
 # start train
