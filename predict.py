7e8d7e52b7b7bc22f098163e2698f3606577eeff
Chandrakant Bothe <crbothe@outlook.com>
2020-03-04 12:26:54 +0100

    predict using given config (under work)

--- predict.py
+++ predict.py
@@ -4,6 +4,10 @@ import sys
 import numpy as np
 from darkflow.net.build import TFNet
 
+from config_options import options, base_data_dir
+
+options = options(base_data_dir, train=False)
+
 
 def boxing(original_img, predictions):
     newImage = np.copy(original_img)
@@ -21,42 +25,40 @@ def boxing(original_img, predictions):
         if confidence > 0.5:
             newImage = cv2.rectangle(
                 newImage, (top_y, top_x), (btm_y, btm_x), (0, 0, 255), 8)
-            newImage = cv2.putText(newImage, label, (top_y, top_x-5),
+            newImage = cv2.putText(newImage, label, (top_y, top_x - 5),
                                    cv2.FONT_HERSHEY_COMPLEX_SMALL, 3, (13, 106, 173), 3, cv2.LINE_AA)
 
     return newImage
 
 
 def predict(source):
-    options = {"model": "cfg/custom-yolov2.cfg",
-               "load": -1,
-               "gpu": 1.0}
     tfnet = TFNet(options)
-    tfnet.load_from_ckpt()
+
     if source == 'image':
-        for image in os.listdir('test-images/'):
-            original_img = cv2.imread("test-images/"+image)
+        path = base_data_dir + '/sample_img/'
+        for image in os.listdir(path):
+            original_img = cv2.imread(path + image)
             original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)
+            tfnet.predict()
             results = tfnet.return_predict(original_img)
             if not results:
                 continue
             print(results)
-            cv2.imwrite('/valohai/outputs/'+'prediction' +
-                        image, boxing(original_img, results))
+            cv2.imwrite(path + 'out/' + 'prediction_' + image, boxing(original_img, results))
     elif source == 'video':  # predict on video
         cap = cv2.VideoCapture('GP010080.mov')
         width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
         height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
         fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-        out = cv2.VideoWriter('/valohai/outputs/GP010080_preds.mov', fourcc,
+        out = cv2.VideoWriter('/outputs/GP010080_preds.mov', fourcc,
                               20.0, (int(width), int(height)))
-        while(True):
+        while True:
             # Capture frame-by-frame
             ret, frame = cap.read()
 
-            if ret == True:
+            if ret:
                 frame = np.asarray(frame)
-                results = tfnet2.return_predict(frame)
+                results = tfnet.return_predict(frame)
                 new_frame = boxing(frame, results)
                 # Display the resulting frame
                 out.write(new_frame)
