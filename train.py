7bf370c1d0a825dd17f072f6f25711f42b9399f3
Chandrakant Bothe <crbothe@outlook.com>
2020-03-04 12:26:29 +0100

    train using given config

--- train.py
+++ train.py
@@ -1,52 +1,14 @@
-import os
-
+from config_options import options, base_data_dir
 from darkflow.net.build import TFNet
 
-# Directory of the data to be trained (should contain: custom-yolov2.cfg, annotations, images, logs, and ckpt)
-base_data_dir = "data_valves"
+options = options(base_data_dir)
 
-# Check if already trained
-if os.path.exists(base_data_dir + "/ckpt/checkpoint"):
-    load = -1
-else:
-    load = "bin/yolov2.weights"
-    if not os.path.exists(load):
-        print("bin/yolov2.weights is not available, initializing weights from scratch!")
-        load = 0
-
-options = {"model": base_data_dir + "/custom-yolov2.cfg",
-           'load': load,
-           "batch": 8,
-           "epoch": 10000,
-           'momentum': 0.9,
-           "trainer": 'adam',
-           'summary': base_data_dir + "/logs/",
-           "backup": base_data_dir + "/ckpt/",
-           "gpu": 1.0,
-           "train": True,
-           "save": 1000,
-           "annotation": base_data_dir + "/annotations/",
-           "dataset": base_data_dir + "/images/",
-           "labels": base_data_dir + "/labels.txt"}
+print("You are training '{}' data with {} batch size for {} epochs and".format(base_data_dir.upper(),
+                                                                               options["batch"],
+                                                                               options["epoch"]))
 
 # load network
 tfnet = TFNet(options)
-# tfnet.savepb()
-
-import tensorflow as tf
-export_dir = "data_valves/built_graph/savedModel"
-tf.saved_model.save(tfnet, export_dir)
-
-# Convert the model.
-converter = tf.lite.TFLiteConverter.from_saved_model(export_dir)
-tflite_model = converter.convert()
-
-
-
-from ext_graph_utils.freeze_graph import freeze_session
-tf.set_learning_phase(0)
-frozen_graph = freeze_session(tf.get_session(), output_names=[out.op.name for out in tfnet.out])
-tf.train.write_graph(frozen_graph, "params/pb", "tf_model.pb", as_text=False)
 
 # start train
 tfnet.train()
